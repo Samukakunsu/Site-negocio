7a62bf0e4dcdec32af968115f246085943b8dcb9
Samuel Costa Souza <131425624+Samukakunsu@users.noreply.github.com>
2025-07-10 17:07:00 -0300

    Add files via upload

---
+++ onepage.js
@@ -0,0 +1,12 @@
+// Rolagem suave para os links do menu
+document.querySelectorAll("a[href^='#']").forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
